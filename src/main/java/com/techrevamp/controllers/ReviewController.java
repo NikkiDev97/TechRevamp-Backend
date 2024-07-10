d52aee4df5fb9f3bf3f9c176f4623c3109798749
Giovanny Salcedo <giovanny.salcedo@genstudents.org>
2024-07-09 22:42:01 -0500

    fix: queries optmization for shipping-addresses

--- src/main/java/com/techrevamp/controllers/ReviewController.java
+++ src/main/java/com/techrevamp/controllers/ReviewController.java
@@ -81,7 +81,7 @@ public class ReviewController {
             if (existingReview == null) {
                 return ResponseEntity.notFound().build();
             }
-            
+
             if (false) {
                 existingReview.setRating(reviewUpdate.getRating());
             }
