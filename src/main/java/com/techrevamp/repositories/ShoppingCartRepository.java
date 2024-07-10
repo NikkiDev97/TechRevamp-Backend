24e2068e560359ecc0ef2dfb9c106ef881fe5460
Giovanny Salcedo <giovanny.salcedo@genstudents.org>
2024-07-09 23:10:26 -0500

    fix: queries optmization for shopping-cart

--- src/main/java/com/techrevamp/repositories/ShoppingCartRepository.java
+++ src/main/java/com/techrevamp/repositories/ShoppingCartRepository.java
@@ -13,16 +13,16 @@ import java.util.List;
 
 @Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
-//  @Query("SELECT p FROM ShoppingCartProduct p WHERE p.shoppingCart.cartId = :cartId")
-//  List<ShoppingCartProduct> findProductsByCartId(@Param("cartId") Long cartId);
+//    @Query("SELECT p FROM ShoppingCartProduct p WHERE p.shoppingCart.cartId = :cartId")
+//    List<ShoppingCartProduct> findProductsByCartId(@Param("cartId") Long cartId);
+
+//    @Modifying
+//    @Transactional
+//    @Query("DELETE FROM ShoppingCartProduct p WHERE p.shoppingCart.cartId = :cartId AND p.product.productId = :productId")
+//    void deleteProductFromCart(@Param("cartId") Long cartId, @Param("productId") Long productId);
 //
-//  @Modifying
-//  @Transactional
-//  @Query("DELETE FROM ShoppingCartProduct p WHERE p.shoppingCart.cartId = :cartId AND p.product.productId = :productId")
-//  void deleteProductFromCart(@Param("cartId") Long cartId, @Param("productId") Long productId);
-//
-//  @Modifying
-//  @Transactional
-//  @Query("UPDATE ShoppingCartProduct p SET p.quantity = :quantity WHERE p.shoppingCart.cartId = :cartId AND p.product.productId = :productId")
-//  void updateProductQuantityInCart(@Param("cartId") Long cartId, @Param("productId") Long productId, @Param("quantity") int quantity);
+//    @Modifying
+//    @Transactional
+//    @Query("UPDATE ShoppingCartProduct p SET p.quantity = :quantity WHERE p.shoppingCart.cartId = :cartId AND p.product.productId = :productId")
+//    void updateProductQuantityInCart(@Param("cartId") Long cartId, @Param("productId") Long productId, @Param("quantity") int quantity);
 }
